f38d13b960fced67fa016f063c799ad79f9b551e
Arto Bendiken <arto@dryproject.org>
2018-04-14 19:26:54 +0300

    Defined the initial set of target languages.

---
+++ src/target.ml
@@ -0,0 +1,88 @@
+(* This is free and unencumbered software released into the public domain. *)
+
+open Dry.Core
+
+(** C *)
+module C = struct
+  (* TODO *)
+end
+
+(** C++ *)
+module Cpp = struct
+  (* TODO *)
+end
+
+(** D *)
+module D = struct
+  (* TODO *)
+end
+
+(** Dart *)
+module Dart = struct
+  (* TODO *)
+end
+
+(** Elixir *)
+module Elixir = struct
+  (* TODO *)
+end
+
+(** Go *)
+module Go = struct
+  (* TODO *)
+end
+
+(** Java *)
+module Java = struct
+  (* TODO *)
+end
+
+(** Julia *)
+module Julia = struct
+  (* TODO *)
+end
+
+(** JS *)
+module JS = struct
+  (* TODO *)
+end
+
+(** Kotlin *)
+module Kotlin = struct
+  (* TODO *)
+end
+
+(** Common Lisp *)
+module Lisp = struct
+  (* TODO *)
+end
+
+(** Lua *)
+module Lua = struct
+  (* TODO *)
+end
+
+(** OCaml *)
+module OCaml = struct
+  (* TODO *)
+end
+
+(** PHP *)
+module PHP = struct
+  (* TODO *)
+end
+
+(** Python *)
+module Python = struct
+  (* TODO *)
+end
+
+(** Ruby *)
+module Ruby = struct
+  (* TODO *)
+end
+
+(** Rust *)
+module Rust = struct
+  (* TODO *)
+end
