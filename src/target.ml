66cec400034edf30364164c488e77d228f9c415c
Arto Bendiken <arto@dryproject.org>
2018-04-17 07:40:49 +0300

    Ensured that unimplemented functions raise errors.

--- src/target.ml
+++ src/target.ml
@@ -8,85 +8,102 @@ end
 
 (** C *)
 module C : Language = struct
-  let compile code buffer = () (* TODO *)
+  let compile code buffer =
+    failwith "not implemented yet" (* TODO *)
 end
 
 (** C++ *)
 module Cpp : Language = struct
-  let compile code buffer = () (* TODO *)
+  let compile code buffer =
+    failwith "not implemented yet" (* TODO *)
 end
 
 (** D *)
 module D : Language = struct
-  let compile code buffer = () (* TODO *)
+  let compile code buffer =
+    failwith "not implemented yet" (* TODO *)
 end
 
 (** Dart *)
 module Dart : Language = struct
-  let compile code buffer = () (* TODO *)
+  let compile code buffer =
+    failwith "not implemented yet" (* TODO *)
 end
 
 (** Elixir *)
 module Elixir : Language = struct
-  let compile code buffer = () (* TODO *)
+  let compile code buffer =
+    failwith "not implemented yet" (* TODO *)
 end
 
 (** Go *)
 module Go : Language = struct
-  let compile code buffer = () (* TODO *)
+  let compile code buffer =
+    failwith "not implemented yet" (* TODO *)
 end
 
 (** Java *)
 module Java : Language = struct
-  let compile code buffer = () (* TODO *)
+  let compile code buffer =
+    failwith "not implemented yet" (* TODO *)
 end
 
 (** Julia *)
 module Julia : Language = struct
-  let compile code buffer = () (* TODO *)
+  let compile code buffer =
+    failwith "not implemented yet" (* TODO *)
 end
 
 (** JS *)
 module JS : Language = struct
-  let compile code buffer = () (* TODO *)
+  let compile code buffer =
+    failwith "not implemented yet" (* TODO *)
 end
 
 (** Kotlin *)
 module Kotlin : Language = struct
-  let compile code buffer = () (* TODO *)
+  let compile code buffer =
+    failwith "not implemented yet" (* TODO *)
 end
 
 (** Common Lisp *)
 module Lisp : Language = struct
-  let compile code buffer = () (* TODO *)
+  let compile code buffer =
+    failwith "not implemented yet" (* TODO *)
 end
 
 (** Lua *)
 module Lua : Language = struct
-  let compile code buffer = () (* TODO *)
+  let compile code buffer =
+    failwith "not implemented yet" (* TODO *)
 end
 
 (** OCaml *)
 module OCaml : Language = struct
-  let compile code buffer = () (* TODO *)
+  let compile code buffer =
+    failwith "not implemented yet" (* TODO *)
 end
 
 (** PHP *)
 module PHP : Language = struct
-  let compile code buffer = () (* TODO *)
+  let compile code buffer =
+    failwith "not implemented yet" (* TODO *)
 end
 
 (** Python *)
 module Python : Language = struct
-  let compile code buffer = () (* TODO *)
+  let compile code buffer =
+    failwith "not implemented yet" (* TODO *)
 end
 
 (** Ruby *)
 module Ruby : Language = struct
-  let compile code buffer = () (* TODO *)
+  let compile code buffer =
+    failwith "not implemented yet" (* TODO *)
 end
 
 (** Rust *)
 module Rust : Language = struct
-  let compile code buffer = () (* TODO *)
+  let compile code buffer =
+    failwith "not implemented yet" (* TODO *)
 end
