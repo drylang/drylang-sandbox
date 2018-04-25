a1c8ef70d526618f31b0d7c8a3fd0c373848747f
Arto Bendiken <arto@dryproject.org>
2018-04-25 09:48:34 +0300

    Added Common Lisp as a target language.

--- src/target/Lisp.ml
+++ src/target/Lisp.ml
@@ -1,9 +1,11 @@
 (* This is free and unencumbered software released into the public domain. *)
 
 module Source = Semantic
-module Target = DRY.Code.Lua (* TODO *)
+module Target = DRY.Code.Lisp
 
 let not_implemented () = failwith "not implemented yet"
+
+let symbol s = Target.symbol (Symbol.of_string s)
 
 let word = function
   | Word.Word8 _ -> not_implemented ()
@@ -11,14 +13,11 @@ let word = function
   | Word.Word32 _ -> not_implemented ()
   | Word.Word64 _ -> not_implemented ()
 
-let number = function
-  | Number.Float _ -> not_implemented ()
-  | Number.Int _ -> not_implemented ()
-  | _ -> not_implemented ()
+let number = Target.number
 
 let scalar = function
-  | Scalar.Bool _ -> not_implemented ()
-  | Scalar.Char _ -> not_implemented ()
+  | Scalar.Bool b -> Target.Object.of_bool b
+  | Scalar.Char c -> Target.character c
   | Scalar.Number n -> number n
   | Scalar.Word w -> word w
 
@@ -28,16 +27,37 @@ let tensor = function
   | Tensor.Matrix _ -> not_implemented ()
 
 let datum = function
-  | Datum.Symbol _ -> not_implemented ()
+  | Datum.Symbol s -> Target.symbol s
   | Datum.Tensor x -> tensor x
   | _ -> not_implemented ()
 
-let compile_node ppf = function
-  | Source.Node.Const x -> Target.print ppf (datum x)
-  | _ -> not_implemented ()
+let rec translate_node = function
+  | Source.Node.Const x -> datum x
+  | Source.Node.Var x -> Target.symbol x
+  | Source.Node.Apply (op, args) -> Target.form ((translate_node op) :: (List.map translate_node args))
+  | Source.Node.Not a -> Target.form [symbol "not"; translate_node a]
+  | Source.Node.And (a, b) -> Target.form [symbol "and"; translate_node a; translate_node b]
+  | Source.Node.Or (a, b) -> Target.form [symbol "or"; translate_node a; translate_node b]
+  | Source.Node.If (a, b, c) -> Target.form [symbol "if"; translate_node a; translate_node b; translate_node c]
+  | Source.Node.Add (a, b) -> Target.form [symbol "+"; translate_node a; translate_node b]
+  | Source.Node.Sub (a, b) -> Target.form [symbol "-"; translate_node a; translate_node b]
+  | Source.Node.Mul (a, b) -> Target.form [symbol "*"; translate_node a; translate_node b]
+  | Source.Node.Div (a, b) -> Target.form [symbol "/"; translate_node a; translate_node b]
 
-let compile_module ppf code =
-  not_implemented ()
+let translate_module (module_ : Source.Module.t) =
+  Target.Program.make (List.map translate_node module_.code)
 
-let compile_program ppf code =
-  not_implemented ()
+let translate_program (program : Source.Program.t) =
+  Target.Program.make (List.map translate_node program.code)
+
+let compile_node ppf node =
+  (* DEBUG *) Format.pp_print_char ppf ';'; Source.Node.print ppf node; Format.pp_print_newline ppf ();
+  translate_node node |> Target.Expression.print ppf
+
+let compile_module ppf module_ =
+  (* DEBUG *) Format.pp_print_char ppf ';'; Source.Module.print ppf module_; Format.pp_print_newline ppf ();
+  translate_module module_ |> Target.Program.print ppf
+
+let compile_program ppf program =
+  (* DEBUG *) Format.pp_print_char ppf ';'; Source.Program.print ppf program; Format.pp_print_newline ppf ();
+  translate_program program |> Target.Program.print ppf
