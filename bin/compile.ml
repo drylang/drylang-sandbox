80f31f05e9cdd1054b2928208c5498d2e0d7f7b7
Arto Bendiken <arto@dryproject.org>
2018-05-02 15:06:00 +0300

    Implemented the dry-compile command.

--- bin/compile.ml
+++ bin/compile.ml
@@ -3,12 +3,45 @@
 open DRY.Core
 open Drylang
 
-let main root options =
-  Printf.eprintf "not implemented yet\n%!" (* TODO *)
+module Stdlib = DRY__Stdlib
+module Format = Stdlib.Format
+
+let warn = Stdlib.Printf.eprintf
+
+let main root (input : SourceFile.t) (output : TargetFile.t) language options =
+  let output_ext = match language with Some s -> s | None -> output.ext in
+  let output_ppf = Format.formatter_of_out_channel output.channel in
+  let input_lexbuf = Lexing.from_channel input.channel in
+  while true do
+    try
+      match Parser.parse_from_lexbuf input_lexbuf with
+      | None -> exit 0
+      | Some syntax ->
+        begin match Target.by_extension output_ext with
+        | None -> assert false
+        | Some (module L : Target.Language) ->
+          let code = Semantic.analyze_program input syntax in
+          L.compile_program output_ppf code;
+          Format.pp_print_newline output_ppf ()
+        end
+    with
+    | Syntax.Error (Lexical, message) ->
+      warn "lexical error: %s\n%!" message;
+      exit 1
+    | Syntax.Error (Syntactic, message) ->
+      warn "syntax error: %s\n%!" message;
+      exit 1
+    | Syntax.Error (Semantic, message) ->
+      warn "semantic error: %s\n%!" message;
+      exit 1
+  done
 
 (* Command-line interface *)
 
 open Cmdliner
+
+let input  = Options.source_file 0 "The input file to compile."
+let output = Options.target_file "The output file name."
 
 let cmd =
   let name = "dry-compile" in
@@ -20,7 +53,7 @@ let cmd =
     `S Manpage.s_bugs; `P "File bug reports at <$(b,https://github.com/dryproject/drylang)>.";
     `S Manpage.s_see_also; `P "$(b,dry)(1), $(b,dry-export)(1)" ]
   in
-  Term.(const main $ Options.package_root $ Options.common),
+  Term.(const main $ Options.package_root $ input $ output $ Options.output_language $ Options.common),
   Term.info name ~version ~doc ~exits ~envs ~man
 
 let () = Term.(exit @@ eval cmd)
